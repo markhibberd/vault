a0039abc6371757aa55fb7817d237473a2de9491
Tony Morris <tmorris@tmorris.net>
2011-01-25 18:08:11 +1000

    Started filling out Connector
    Former-commit-id: ef838f3896ecd14d2829f38a14b2992cd3141a82

--- src/prod/com/ephox/vault2/Connector.scala
+++ src/prod/com/ephox/vault2/Connector.scala
@@ -1,8 +1,23 @@
 package com.ephox.vault2
 
 import java.sql.Connection
-
+import scalaz._
+import Scalaz._
 
 sealed trait Connector[M[_], A] {
   val connect: Connection => M[SQLValue[A]]
+
+  def apply(c: Connection) =
+    connect(c)
+}
+
+object Connector {
+  implicit def ConnectorFunctor[M[_]](implicit ff: Functor[M]): Functor[({type λ[α]=Connector[M, α]})#λ] = new Functor[({type λ[α]=Connector[M, α]})#λ] {
+    def fmap[A, B](k: Connector[M, A], f: A => B) = new Connector[M, B] {
+      val connect = (c: Connection) => {
+        val aa: M[SQLValue[A]] = k(c)
+        val aaa = k(c) map (z => z map (y => y))
+        error("") }
+    }
+  }
 }
