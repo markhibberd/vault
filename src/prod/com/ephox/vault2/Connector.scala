368348744f85dfb8f17bce4706fb2b04db00cf15
Tony Morris <tmorris@tmorris.net>
2011-01-25 18:15:09 +1000

    Filling out Connector

--- src/prod/com/ephox/vault2/Connector.scala
+++ src/prod/com/ephox/vault2/Connector.scala
@@ -9,12 +9,21 @@ sealed trait Connector[M[_], A] {
 
   def apply(c: Connection) =
     connect(c)
+
+  def toKleisli:Kleisli[M, Connection, SQLValue[A]] =
+    ☆(connect)
+
 }
 
 object Connector {
+  def connector[M[_], A](f: Connection => M[SQLValue[A]]): Connector[M, A] = new Connector[M, A] {
+    val connect = f
+  }
+
   implicit def ConnectorFunctor[M[_]](implicit ff: Functor[M]): Functor[({type λ[α]=Connector[M, α]})#λ] = new Functor[({type λ[α]=Connector[M, α]})#λ] {
     def fmap[A, B](k: Connector[M, A], f: A => B) = new Connector[M, B] {
       val connect = (c: Connection) => k(c) map (_ map f)
     }
   }
+
 }
