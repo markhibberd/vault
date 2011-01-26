26a0ede0aec32d5e252f242cd2b41d4632cd31d5
Tony Morris <code@tmorris.net>
2011-01-27 08:14:24 +1000

    Rename

--- src/prod/com/ephox/vault2/ConnectorM.scala
+++ src/prod/com/ephox/vault2/ConnectorM.scala
@@ -16,31 +16,31 @@ sealed trait ConnectorM[M[_], A] {
 }
 
 object ConnectorM {
-  def connector[M[_], A](f: Connection => M[SQLValue[A]]): ConnectorM[M, A] = new ConnectorM[M, A] {
+  def connectorM[M[_], A](f: Connection => M[SQLValue[A]]): ConnectorM[M, A] = new ConnectorM[M, A] {
     val connect = f
   }
 
-  implicit def ConnectorFunctor[M[_]](implicit ff: Functor[M]): Functor[({type λ[α]=ConnectorM[M, α]})#λ] = new Functor[({type λ[α]=ConnectorM[M, α]})#λ] {
+  implicit def ConnectorMFunctor[M[_]](implicit ff: Functor[M]): Functor[({type λ[α]=ConnectorM[M, α]})#λ] = new Functor[({type λ[α]=ConnectorM[M, α]})#λ] {
     def fmap[A, B](k: ConnectorM[M, A], f: A => B) =
-      connector((c: Connection) => k(c) map (_ map f))
+      connectorM((c: Connection) => k(c) map (_ map f))
   }
 
-  implicit def ConnectorPure[M[_]](implicit pp: Pure[M]): Pure[({type λ[α]=ConnectorM[M, α]})#λ] = new Pure[({type λ[α]=ConnectorM[M, α]})#λ] {
+  implicit def ConnectorMPure[M[_]](implicit pp: Pure[M]): Pure[({type λ[α]=ConnectorM[M, α]})#λ] = new Pure[({type λ[α]=ConnectorM[M, α]})#λ] {
     def pure[A](a: => A) =
-      connector(_ => a.η[SQLValue].η[M])
+      connectorM(_ => a.η[SQLValue].η[M])
   }
 
-  implicit def ConnectorApply[M[_]](implicit app: Apply[M], ftr: Functor[M]): Apply[({type λ[α]=ConnectorM[M, α]})#λ] = new Apply[({type λ[α]=ConnectorM[M, α]})#λ] {
+  implicit def ConnectorMApply[M[_]](implicit app: Apply[M], ftr: Functor[M]): Apply[({type λ[α]=ConnectorM[M, α]})#λ] = new Apply[({type λ[α]=ConnectorM[M, α]})#λ] {
     def apply[A, B](f: ConnectorM[M, A => B], a: ConnectorM[M, A]) =
-      connector(c => f(c).<**>(a(c))(SQLValue.SQLValueApply(_, _)))
+      connectorM(c => f(c).<**>(a(c))(SQLValue.SQLValueApply(_, _)))
   }
 
-  implicit def ConnectorMonad[M[_]](implicit mnd: Monad[M]): Monad[({type λ[α]=ConnectorM[M, α]})#λ] = new Monad[({type λ[α]=ConnectorM[M, α]})#λ] {
+  implicit def ConnectorMMonad[M[_]](implicit mnd: Monad[M]): Monad[({type λ[α]=ConnectorM[M, α]})#λ] = new Monad[({type λ[α]=ConnectorM[M, α]})#λ] {
     def bind[A, B](a: ConnectorM[M, A], f: A => ConnectorM[M, B]) =
-      connector((c: Connection) =>
+      connectorM((c: Connection) =>
         a(c) >>= ((z: SQLValue[A]) =>
           z fold (e => SQLValue.err(e).pure[M], a => f(a).connect(c))))
 
-    def pure[A](a: => A) = connector(_ => a.η[SQLValue].η[M])
+    def pure[A](a: => A) = connectorM(_ => a.η[SQLValue].η[M])
   }
 }
