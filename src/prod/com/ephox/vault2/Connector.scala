abb01bf8fb855e7908113df394f98ee8ad289af8
Tony Morris <code@tmorris.net>
2011-01-27 08:38:46 +1000

    non-monadic version of connector

---
+++ src/prod/com/ephox/vault2/Connector.scala
@@ -0,0 +1,40 @@
+package com.ephox.vault2
+
+import java.sql.Connection
+import scalaz._
+import Scalaz._
+
+
+sealed trait Connector[A] {
+  val connect: Connection => SQLValue[A]
+
+  def apply(c: Connection) = connect(c)
+}
+
+object Connector {
+  import SQLValue._
+
+  def connector[A](f: Connection => SQLValue[A]): Connector[A] = new Connector[A] {
+    val connect = f
+  }
+
+  implicit def ConnectorFunctor: Functor[Connector] = new Functor[Connector] {
+    def fmap[A, B](k: Connector[A], f: A => B) =
+      connector((c: Connection) => k(c) map f)
+  }
+
+  implicit def ConnectorPure[M[_]]: Pure[Connector] = new Pure[Connector] {
+    def pure[A](a: => A) =
+      connector(_ => a.η[SQLValue])
+  }
+
+  implicit def ConnectorApply[M[_]]: Apply[Connector] = new Apply[Connector] {
+    def apply[A, B](f: Connector[A => B], a: Connector[A]) =
+      connector(c => a(c) <*> f(c))
+  }
+
+  implicit def ConnectorBind[M[_]]: Bind[Connector] = new Bind[Connector] {
+    def bind[A, B](a: Connector[A], f: A => Connector[B]) =
+      connector(c => a(c) >>= (a => f(a)(c)))
+  }
+}
