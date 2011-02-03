c9c4aaa10ab72a43e3c39b946f3aa155858d7d1c
Tony Morris <tmorris@tmorris.net>
2011-02-03 12:22:05 +1000

    Tidy up multiple PreparedStatement executions

--- src/prod/com/ephox/vault2/PreparedStatementW.scala
+++ src/prod/com/ephox/vault2/PreparedStatementW.scala
@@ -1,12 +1,25 @@
 package com.ephox.vault2
 
 import java.sql.PreparedStatement
+import scalaz._
+import Scalaz._
 
 sealed trait PreparedStatementW {
   val s: PreparedStatement
 
   def tryExecuteUpdate: SQLValue[Int] =
     tryValue(s.executeUpdate)
+
+  def executeStatements[T[_], A](as: T[A], k: A => Connector[Unit])(implicit f: Foldable[T]): Connector[Int] =
+    connector(c => as.foldLeftM(0) {
+       case (n, a) => {
+         k(a)(c)
+         s.tryExecuteUpdate ∘ (n+)
+       }
+     })
+
+  def foreachStatement[T[_], A](as: T[A], k: A => Unit)(implicit f: Foldable[T]): Connector[Int] =
+    executeStatements(as, (a: A) => k(a).η[Connector])
 }
 
 object PreparedStatementW {
