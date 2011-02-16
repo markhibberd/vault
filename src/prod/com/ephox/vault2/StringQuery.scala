d75d2c46eaf5a40bd972735c43e4891ac3bb9dab
Tony Morris <tmorris@tmorris.net>
2011-02-16 12:15:19 +1000

    no need for resultset connector anymore
    Former-commit-id: a86bea9a70506a4a458474757258f2730973906c

--- src/prod/com/ephox/vault2/StringQuery.scala
+++ src/prod/com/ephox/vault2/StringQuery.scala
@@ -1,19 +1,9 @@
 package com.ephox.vault2
 
-import java.sql.{PreparedStatement, ResultSet, Statement}
+import java.sql.{PreparedStatement, Statement}
 
 sealed trait StringQuery {
   val sql: String
-
-  def executeQuery[A](f: ResultSetConnector[A]): Connector[A] =
-    connector(c => withSQLResource(
-                     value = c prepareStatement sql
-                   , evaluate = (s: PreparedStatement) =>
-                       withSQLResource(
-                         value    = s.executeQuery
-                       , evaluate = (r: ResultSet) => f(r)(c)
-                       )
-                   ))
 
   def executeUpdate[A]: Connector[Int] =
     connector(c => withSQLResource(
