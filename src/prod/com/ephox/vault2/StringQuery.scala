25c0acec028915a5fbb925e627aaf7d27f79e1ee
Tony Morris <tmorris@tmorris.net>
2011-02-16 12:56:42 +1000

    not totally happy with this signature to executeUpdate then with keys

--- src/prod/com/ephox/vault2/StringQuery.scala
+++ src/prod/com/ephox/vault2/StringQuery.scala
@@ -1,5 +1,7 @@
 package com.ephox.vault2
 
+import scalaz._
+import Scalaz._
 import java.sql.{PreparedStatement, Statement}
 
 sealed trait StringQuery {
@@ -12,17 +14,28 @@ sealed trait StringQuery {
                        tryValue(s executeUpdate sql)
                    ))
 
-  def executeUpdateWithKeys[A](f: Row => A): Connector[(Int, A)] =
+  def executeUpdateWithKeys[A, B](withStatement: PreparedStatement => Connector[A], withRow: Row => A => Int => Connector[B]): Connector[B] =
     connector(c => withSQLResource(
                      value = c.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)
                    , evaluate = (s: PreparedStatement) => {
-                       tryValue({
-                         val c = s.executeUpdate
-                         val k = s.getGeneratedKeys
-                         (c, f(Row.resultSetRow(k)))
-                       })
+                         val o = for {
+                           i <- withStatement(s)
+                           b <- {
+                             val z = s.executeUpdate
+                             val k = s.getGeneratedKeys
+                             withRow(Row.resultSetRow(k))(i)(z)
+                           }
+                         } yield b
+                         o(c)
                      }
                    ))
+
+  def executeUpdateWithKeysSet[A, B](withStatement: PreparedStatement => Unit, withRow: Row => Int => B): Connector[B] =
+    executeUpdateWithKeys(
+      withStatement = withStatement(_).η[Connector]
+    , withRow       = (r: Row) => (_: Unit) => (n: Int) => withRow(r)(n).η[Connector]
+    )
+
 
   def prepareStatement[A](k: PreparedStatement => Connector[A]) : Connector[A] =
     connector(c => withSQLResource(c prepareStatement sql, (s: PreparedStatement) => k(s)(c)))
