06a9c33cf79cb26d85b4e9948aa7f09653bb6bf1
Tony Morris <tmorris@tmorris.net>
2011-03-25 16:45:35 +1000

    Recovering functionality after introducing logging

--- src/prod/com/ephox/vault/DDL.scala
+++ src/prod/com/ephox/vault/DDL.scala
@@ -4,7 +4,6 @@ import scalaz._
 import Scalaz._
 
 trait DDL {
-  // todo trick to apply only one of the two parameters
   def executeUpdates[F[_], L](sqls: F[String])(implicit t: Traverse[F], fld: Foldable[F]): SqlConnect[L, Int] = {
     sqls.traverse[({type λ[α]= SqlConnect[L, α]})#λ, Int](_.executeUpdate) ∘ (_.sum)
   }
