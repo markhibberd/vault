e02ae066c175bb94c4e56876286aad3eb73cb097
Tony Morris <tmorris@tmorris.net>
2011-03-25 15:56:59 +1000

    Recovering functionality after introducing logging
    Former-commit-id: a71c86f2ee477956ceed8a52f8b39b0065fcb57d

--- src/prod/com/ephox/vault/SqlConnect.scala
+++ src/prod/com/ephox/vault/SqlConnect.scala
@@ -58,45 +58,43 @@ sealed trait SqlConnect[L, A] {
 }
 
 trait SqlConnects {
-  def sqlConnect[L, A](f: Connection => SqlValue[L, A]): SqlConnect[L, A] = /*new SqlConnect[A] {
+  def sqlConnect[L, A](f: Connection => SqlValue[L, A]): SqlConnect[L, A] = new SqlConnect[L, A] {
     val connect = f
-  } */
-    error("todo")
+  }
 
   def constantSqlConnect[L, A](v: => SqlValue[L, A]): SqlConnect[L, A] =
-    // sqlConnect(_ => v)
-    error("todo")
+    sqlConnect(_ => v)
 
   def valueSqlConnect[L, A](f: Connection => A): SqlConnect[L, A] =
-    // sqlConnect(f(_).η[SqlValue])
-    error("todo")
+    sqlConnect(f(_).η[({type λ[α]= SqlValue[L, α]})#λ])
 
   def trySqlConnect[L, A](f: Connection => A): SqlConnect[L, A] =
-    // sqlConnect(c => trySqlValue(f(c)))
-    error("todo")
+    sqlConnect(c => trySqlValue(f(c)))
 
   def closeSqlConnect[L]: SqlConnect[L, Unit] =
-    // trySqlConnect(_.close)
-    error("todo")
+    trySqlConnect(_.close)
 
-//  implicit def SqlConnectFunctor: Functor[SqlConnect] = new Functor[SqlConnect] {
-//    def fmap[A, B](k: SqlConnect[A], f: A => B) =
-//      sqlConnect((c: Connection) => k(c) map f)
-//  }
-//
-//  implicit def SqlConnectPure[M[_]]: Pure[SqlConnect] = new Pure[SqlConnect] {
-//    def pure[A](a: => A) =
-//      valueSqlConnect(_ => a)
-//  }
-//
-//  implicit def SqlConnectApply[M[_]]: Apply[SqlConnect] = new Apply[SqlConnect] {
-//    def apply[A, B](f: SqlConnect[A => B], a: SqlConnect[A]) = {
-//      sqlConnect(c => a(c) <*> f(c))
-//    }
-//  }
-//
-//  implicit def SqlConnectBind[M[_]]: Bind[SqlConnect] = new Bind[SqlConnect] {
-//    def bind[A, B](a: SqlConnect[A], f: A => SqlConnect[B]) =
-//      sqlConnect(c => a(c) >>= (a => f(a)(c)))
-//  }
+  implicit def SqlConnectFunctor[L]: Functor[({type λ[α]= SqlConnect[L, α]})#λ] = new Functor[({type λ[α]= SqlConnect[L, α]})#λ] {
+    def fmap[A, B](k: SqlConnect[L, A], f: A => B) =
+      k map f
+  }
+
+  implicit def SqlConnectPure[L, M[_]]: Pure[({type λ[α]= SqlConnect[L, α]})#λ] = new Pure[({type λ[α]= SqlConnect[L, α]})#λ] {
+    def pure[A](a: => A) =
+      valueSqlConnect(_ => a)
+  }
+
+  implicit def SqlConnectApply[L, M[_]]: Apply[({type λ[α]= SqlConnect[L, α]})#λ] = new Apply[({type λ[α]= SqlConnect[L, α]})#λ] {
+    def apply[A, B](f: SqlConnect[L, A => B], a: SqlConnect[L, A]) = {
+      sqlConnect(c => {
+        val fc = f(c)
+        a(c) <*> fc
+      })
+    }
+  }
+
+  implicit def SqlConnectBind[L, M[_]]: Bind[({type λ[α]= SqlConnect[L, α]})#λ] = new Bind[({type λ[α]= SqlConnect[L, α]})#λ] {
+    def bind[A, B](a: SqlConnect[L, A], f: A => SqlConnect[L, B]) =
+      sqlConnect(c => a(c) >>= (a => f(a)(c)))
+  }
 }
