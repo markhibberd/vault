ad9b5204c0756c9ec0d93aaf48a149c71a0c556f
Tony Morris <tmorris@tmorris.net>
2011-03-28 14:45:09 +1000

    rename
    Former-commit-id: eb046e8a81ce5cbff7943eb21dff32f9170473ab

--- src/prod/com/ephox/vault/RowQuery.scala
+++ src/prod/com/ephox/vault/RowQuery.scala
@@ -7,14 +7,14 @@ sealed trait RowQuery[L, A] {
   def <|-(sql: Query): RowConnect[L, A]
 
   def map[B](f: A => B): RowQuery[L, B] =
-    rowQuerys(s => this <|- s map f)
+    rowQuery(s => this <|- s map f)
 
   def flatMap[B](f: A => RowQuery[L, B]): RowQuery[L, B] =
-    rowQuerys(s => (this <|- s) flatMap (a => f(a) <|- s))
+    rowQuery(s => (this <|- s) flatMap (a => f(a) <|- s))
 }
 
 trait RowQuerys {
-  def rowQuerys[L, A](f: Query => RowConnect[L, A]): RowQuery[L, A] = new RowQuery[L, A] {
+  def rowQuery[L, A](f: Query => RowConnect[L, A]): RowQuery[L, A] = new RowQuery[L, A] {
     def <|-(sql: Query) =
       f(sql)
   }
@@ -26,12 +26,12 @@ trait RowQuerys {
 
   implicit def RowQueryPure[L, M[_]]: Pure[({type λ[α]= RowQuery[L, α]})#λ] = new Pure[({type λ[α]= RowQuery[L, α]})#λ] {
     def pure[A](a: => A) =
-      rowQuerys(_ => a.η[({type λ[α]= RowConnect[L, α]})#λ])
+      rowQuery(_ => a.η[({type λ[α]= RowConnect[L, α]})#λ])
   }
 
   implicit def RowQueryApply[L, M[_]]: Apply[({type λ[α]= RowQuery[L, α]})#λ] = new Apply[({type λ[α]= RowQuery[L, α]})#λ] {
     def apply[A, B](f: RowQuery[L, A => B], a: RowQuery[L, A]) = {
-      rowQuerys(s => (a <|- s) <*> (f <|- s))
+      rowQuery(s => (a <|- s) <*> (f <|- s))
     }
   }
 
