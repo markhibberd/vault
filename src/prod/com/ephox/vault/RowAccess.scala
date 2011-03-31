1458daaabee9add8af45f99d0f5fe55b9f9c6f1a
Tony Morris <tmorris@tmorris.net>
2011-03-31 11:49:35 +1000

    gradually removing package object

--- src/prod/com/ephox/vault/RowAccess.scala
+++ src/prod/com/ephox/vault/RowAccess.scala
@@ -6,6 +6,7 @@ import Scalaz._
 sealed trait RowAccess[L, A] {
   val access: Row => RowValue[L, A]
   import SqlValue._
+  import RowConnect._
 
   def -|>[T](iter: IterV[A, T]): RowQueryConnect[L, IterV[A, T]] =
     rowQueryConnect(query => rowConnect(c => try {
