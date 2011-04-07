79a7dc0df2e16cf13483e754ed8ecbcb2e4ca156
Tony Morris <tmorris@tmorris.net>
2011-04-08 09:21:16 +1000

    add context to setting JDBCType
    Former-commit-id: 871a10a790e02fe7e2816e30b8ef2763c9a001db

--- src/prod/com/ephox/vault/PreparedStatementW.scala
+++ src/prod/com/ephox/vault/PreparedStatementW.scala
@@ -3,6 +3,7 @@ package com.ephox.vault
 import java.sql.PreparedStatement
 import scalaz._, Scalaz._
 import SqlValue._
+import PreparedStatementContext._
 
 sealed trait PreparedStatementW {
   val s: PreparedStatement
@@ -62,48 +63,9 @@ sealed trait PreparedStatementW {
                 , s.setURL(n, _)
                 )
           n + 1
-        })
+        }) mapError (_.setPreparedStatementContext(preparedStatementContextPS(s, v, n)))
       }
     } map (_ => ())
-
-  //   def foldLeftM[N[_], B](b: B)(f: (B, A) => N[B])(implicit fr: Foldable[M], m: Monad[N]): N[B] =
-  /*
-    values.foldl(1) {
-      case (n, v) => {
-        v.fold(
-                t => s.setNull(n, t.toType)
-              , s.setBoolean(n, _)
-              , s.setByte(n, _)
-              , s.setShort(n, _)
-              , s.setInt(n, _)
-              , s.setLong(n, _)
-              , s.setFloat(n, _)
-              , s.setDouble(n, _)
-              , s.setBigDecimal(n, _)
-              , s.setString(n, _)
-              , s.setBytes(n, _)
-              , s.setDate(n, _)
-              , s.setTime(n, _)
-              , s.setTimestamp(n, _)
-              , a => length => s.setAsciiStream(n, a, length)
-              , a => length => s.setBinaryStream(n, a, length)
-              , a => t => s.setObject(n, a, t.toType)
-              , s.setObject(n, _)
-              , a => length => s.setCharacterStream(n, a, length)
-              , s.setRef(n, _)
-              , s.setBlob(n, _)
-              , s.setClob(n, _)
-              , s.setArray(n, _)
-              , a => cal => s.setDate(n, a, cal)
-              , a => cal => s.setTime(n, a, cal)
-              , a => cal => s.setTimestamp(n, a, cal)
-              , t => m => s.setNull(n, t.toType, m)
-              , s.setURL(n, _)
-              )
-        n + 1
-      }
-    }
-    */
 }
 
 object PreparedStatementW extends PreparedStatementWs
