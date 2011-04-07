0807dffbfa1d47de9e3ce7be5c9347a56825d8f0
Tony Morris <tmorris@tmorris.net>
2011-04-07 17:04:31 +1000

    combinators

--- src/prod/com/ephox/vault/SqlExceptionContext.scala
+++ src/prod/com/ephox/vault/SqlExceptionContext.scala
@@ -6,6 +6,18 @@ sealed trait SqlExceptionContext {
   val sqlException: SqlException
 
   val prepareStatementContext: Option[PreparedStatementContext]
+
+  def setPreparedStatementContext(c: PreparedStatementContext) =
+    sqlExceptionContextPS(sqlException, c)
+
+  def unsetPreparedStatementContext =
+    sqlExceptionContext(sqlException)
+
+  def withPreparedStatementContext(k: PreparedStatementContext => PreparedStatementContext) =
+    prepareStatementContext match {
+      case None    => this
+      case Some(c) => setPreparedStatementContext(k(c))
+    }
 }
 
 object SqlExceptionContext extends SqlExceptionContexts
