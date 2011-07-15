7285ae37175b5cc25c1aab11fdb6ce016a8b5133
Tony Morris <tmorris@tmorris.net>
2011-07-15 15:45:11 +1000

    Keyed == Lens
    Former-commit-id: 32d6f9fc81522d76ef32c27b9cb7dae3396bc308

--- src/prod/com/ephox/vault/Keyed.scala
+++ src/prod/com/ephox/vault/Keyed.scala
@@ -2,9 +2,12 @@ package com.ephox.vault
 
 import scalaz._, Scalaz._
 
+// ≈ Lens[A, Key]
 sealed trait Keyed[A] {
   def get(a: A): Key
   def set(a: A, key: Key): A
+  def lens: Lens[A, Key] =
+    Lens(get(_), set(_, _))
 }
 
 object Keyed extends Keyeds
@@ -14,6 +17,9 @@ trait Keyeds {
     def get(a: A) = getf(a)
     def set(a: A, key: Key) = setf(a, key)
   }
+
+  def keyedLens[A](k: Lens[A, Key]): Keyed[A] =
+    keyed(k.get, k.set)
 
   implicit def KeyKeyed: Keyed[Key] =
     keyed(z => z, (_, k) => k)
