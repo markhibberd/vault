56b60fa5f8ec748b02ac988cc70c3e303c053ae5
Tony Morris <tmorris@tmorris.net>
2011-08-18 14:12:27 +1000

    count function ported from haskell
    Former-commit-id: 8d8b5a116d707f33188b8a97a73fa8c3247f371b

--- src/prod/com/ephox/vault/Count.scala
+++ src/prod/com/ephox/vault/Count.scala
@@ -48,31 +48,3 @@ trait Counts {
     }, v)
   }
 }
-
-/*
-condense ::
-  Ord a =>
-  Int
-  -> [Data a]
-  -> ([Data a], Maybe Int)
-condense n d =
-  let u = foldr (\(Datum s i, x) (m, k) ->
-            if M.size m == n - 1 || n < 1
-              then let w = fmap (+ i) k `mplus` Just i
-                   in case M.minViewWithKey m of
-                             Nothing             -> (m, w)
-                             Just ((s', (i', x')), m') -> if i' < i
-                                                            then (M.insert s (i, x) m', fmap (+ i') k `mplus` Just i')
-                                                            else (m, w)
-              else (M.insert s (i, x) m, Nothing)) (M.empty, Nothing) (d `zip` [0..])
-  in first (fmap (uncurry $ \k -> Datum k . fst) . sortBy (comparing (snd . snd)) . M.toList) u
-
-test =
-  [
-    "Bar"  `Datum` 2
-  , "Fred" `Datum` 7
-  , "Baz"  `Datum` 4
-  , "Bob"  `Datum` 5
-  ]
-
-*/
