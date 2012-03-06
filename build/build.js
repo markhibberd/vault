57db276efde70759bfa62eb07f7314df51c6293c
Tony Morris <tmorris@tmorris.net>
2012-03-06 13:54:51 +1000

    ent config for sbt
    Former-commit-id: 6b1723e9d2d53f17b520b924635a73a067fd4da5

--- build/build.js
+++ build/build.js
@@ -1,9 +1,14 @@
 var p = Ent.Project.create("vault", "external");
 p.setVersion(2, 0, 0);
 
+function getVersionString() {
+    var v = p.version;
+    return "set version := \"" + [v.major, v.minor, v.point, v.buildNumber].join(".") + "\"";
+}
 
 p.setConfig({
-    command: ["make"],
-    dist: "gen/dist",
+    command: ["./sbt", getVersionString, "clean", "update", "compile", "test", "package"],
+    dist: "target/scala_2.9.1",
     distInclude: "*.jar"
 });
+
