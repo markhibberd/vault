7b3928217faa188dc3f10ff407b87cb944c6ce87
Tony Morris <tmorris@tmorris.net>
2012-04-04 11:27:32 +1000

    use dent for scalaz dependency
    Former-commit-id: b1f558b3a9dfe7fac6800f3ad9285ade719cd748

--- project/build.scala
+++ project/build.scala
@@ -2,14 +2,6 @@ import sbt._
 import Keys._
 
 object build extends Build {
-  val buildServer = "http://jenkins/job"
-  val scalaz7Version = "3"
-
-  def scalaz7Core(scalaVersion: String) = "scalaz7_core" % "scalaz7_core" % scalaz7Version from "%s/Scalaz7/%s/artifact/core/target/scala-%s/scalaz-core_%s-7.0-SNAPSHOT.jar".format(buildServer, scalaz7Version, scalaVersion, scalaVersion)
-
-  def scalaz7Iteratee(scalaVersion: String) = "scalaz7_iteratee" % "scalaz7_iteratee" % scalaz7Version from "%s/Scalaz7/%s/artifact/iteratee/target/scala-%s/scalaz-iteratee_%s-7.0-SNAPSHOT.jar".format(buildServer, scalaz7Version, scalaVersion, scalaVersion)
-
-  def scalaz7Effect(scalaVersion: String) = "scalaz7_effect" % "scalaz7_effect" % scalaz7Version from "%s/Scalaz7/%s/artifact/effect/target/scala-%s/scalaz-effect_%s-7.0-SNAPSHOT.jar".format(buildServer, scalaz7Version, scalaVersion, scalaVersion)
 
   type Sett = Project.Setting[_]
 
@@ -29,9 +21,6 @@ object build extends Build {
         "org.scala-tools.testing" %% "scalacheck" % "1.9" % "test" withSources
       , "org.scalatest" %% "scalatest" % "1.6.1" % "test" withSources
       , "org.hsqldb" % "hsqldb" % "2.0.0"
-      , scalaz7Core("2.9.1")
-      , scalaz7Iteratee("2.9.1")
-      , scalaz7Effect("2.9.1")
       )
     )
   )
@@ -50,7 +39,6 @@ object build extends Build {
       , "-unchecked"
       )
     , libraryDependencies ++= Seq(
-      // , "org.scalaz" %% "scalaz-core" % "6.0.4" withSources
         "org.hsqldb" % "hsqldb" % "2.0.0"
       )
     )
