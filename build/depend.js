186c4d0fe2a0e4dea325327dd4900dbdd53cf224
Tony Morris <tmorris@tmorris.net>
2012-04-04 11:33:09 +1000

    use dent for scalaz dependency
    Former-commit-id: f9107664f5d2c51cfdc0a2b6e82365479bddcf01

--- build/depend.js
+++ build/depend.js
@@ -13,8 +13,8 @@ var dependencies = [
         source : "scala-2.9.1.final.zip",
         targets : [
             { name : "scala-2.9.1.final/doc/LICENSE", path : licenses + "scala" },
-            { name : "scala-2.9.1.final/lib/scala-library.jar", path : "lib/run" },
-            { name : "scala-2.9.1.final/src/scala-library-src.jar", path : "lib/run" },
+            { name : "scala-2.9.1.final/lib/scala-library.jar", path : "lib" },
+            { name : "scala-2.9.1.final/src/scala-library-src.jar", path : "lib" },
             { name : "scala-2.9.1.final/*", path : "lib/tools/scala" }
         ],
         executables : [
@@ -30,7 +30,7 @@ var dependencies = [
         version : "7/" + scalazV,
         source: "./",
         targets: [
-            { name: "*.jar", path: "lib/run/scalaz/7" }
+            { name: "*.jar", path: "lib" }
         ]
     },
     { name: "scalaz",
@@ -47,14 +47,14 @@ var dependencies = [
         repository: "thirdpartyrepo",
         source: "db2jcc.jar",
         targets: [
-            { name: "db2jcc.jar", path: "lib/test" }
+            { name: "db2jcc.jar", path: "lib" }
         ]
     },
     { name: "db2",
         repository: "thirdpartyrepo",
         source: "db2jcc_license_cisuz.jar",
         targets: [
-            { name: "db2jcc_license_cisuz.jar", path: "lib/test" }
+            { name: "db2jcc_license_cisuz.jar", path: "lib" }
         ]
     },
     { name : "hsqldb",
@@ -62,7 +62,7 @@ var dependencies = [
         source : "hsqldb*.zip",
         version:    "2.0.0/2.0.0.0",
         targets : [
-            {name : "hsqldb/lib/hsqldb.jar", path : "lib/run"}
+            {name : "hsqldb/lib/hsqldb.jar", path : "lib"}
         ]
     },
     { name : "jtds",
@@ -70,7 +70,7 @@ var dependencies = [
         source : "jtds-1.2.5.jar",
         version:    "1.2.5",
         targets : [
-            {name : "jtds-1.2.5.jar", path : "lib/test"}
+            {name : "jtds-1.2.5.jar", path : "lib"}
         ]
     },
     { name: "oracle",
@@ -78,7 +78,7 @@ var dependencies = [
         version: "5",
         source: "ojdbc5.jar",
         targets: [
-            { name: "ojdbc5.jar", path: "lib/test" }
+            { name: "ojdbc5.jar", path: "lib" }
         ]
     },
     { name : "scalacheck",
@@ -86,7 +86,7 @@ var dependencies = [
         version : "2.9.0-1.9",
         source : "scalacheck_2.9.0-1.9.jar",
         targets : [
-            { name : "scalacheck_2.9.0-1.9.jar", path : "lib/test" }
+            { name : "scalacheck_2.9.0-1.9.jar", path : "lib" }
         ]
     },
     { name : "scalacheck",
@@ -94,7 +94,7 @@ var dependencies = [
         version : "2.9.0-1.9",
         source : "scalacheck_2.9.0-1.9-sources.jar",
         targets : [
-            { name : "scalacheck_2.9.0-1.9-sources.jar", path : "lib/test" }
+            { name : "scalacheck_2.9.0-1.9-sources.jar", path : "lib" }
         ]
     },
     { name : "scalatest",
@@ -102,7 +102,7 @@ var dependencies = [
         version : "2.9.1-1.6.1",
         source : "scalatest_2.9.1-1.6.1.jar",
         targets : [
-            { name : "scalatest_2.9.1-1.6.1.jar", path : "lib/test" }
+            { name : "scalatest_2.9.1-1.6.1.jar", path : "lib" }
         ]
     },
     { name : "specs2",
@@ -110,7 +110,7 @@ var dependencies = [
         version : "2.9.1-1.6",
         source : "specs2_2.9.1-1.6.jar",
         targets : [
-            { name : "specs2_2.9.1-1.6.jar", path : "lib/test" }
+            { name : "specs2_2.9.1-1.6.jar", path : "lib" }
         ]
     },
     { name : "specs2",
@@ -118,7 +118,7 @@ var dependencies = [
         version : "2.9.1-1.6",
         source : "specs2_2.9.1-1.6-sources.jar",
         targets : [
-            { name : "specs2_2.9.1-1.6-sources.jar", path : "lib/test" }
+            { name : "specs2_2.9.1-1.6-sources.jar", path : "lib" }
         ]
     },
     { name : "specs2",
@@ -126,7 +126,7 @@ var dependencies = [
         version : "2.9.1-1.6",
         source : "specs2-scalaz-core_2.9.1-6.0.1.jar",
         targets : [
-            { name : "specs2-scalaz-core_2.9.1-6.0.1.jar", path : "lib/test" }
+            { name : "specs2-scalaz-core_2.9.1-6.0.1.jar", path : "lib" }
         ]
     }
 ];
