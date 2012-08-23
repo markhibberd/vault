83802c4a3704a0d8c5e7fce50ac7bd711de93c5b
Tony Morris <tmorris@tmorris.net>
2012-08-23 11:02:31 +1000

    Sql
    Former-commit-id: 9dd70ad817b192e0ccaa3ab46879d64dd824518a

---
+++ src/main/scala/com/ephox/vault/Sql.scala
@@ -0,0 +1,24 @@
+package com.ephox
+package vault
+
+import scalaz._, Scalaz._
+
+sealed trait Sql {
+  val sql: String
+  val bindings: List[JDBCType]
+}
+
+object Sql extends SqlFunctions
+
+trait SqlFunctions {
+  def query(s: String, b: List[JDBCType]): Sql = new Sql {
+    val sql = s
+    val bindings = b
+  }
+
+  def sqlL: Sql @> String =
+    Lens(s => Store(query(_, s.bindings), s.sql))
+
+  def bindingsL: Sql @> List[JDBCType] =
+    Lens(s => Store(query(s.sql, _), s.bindings))
+}
