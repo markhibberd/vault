65e52bf3a2defa0b1d7cc8fe7f3790b9fb59bb4b
Tony Morris <tmorris@tmorris.net>
2012-08-27 15:23:29 +1000

    transformer stack
    Former-commit-id: cad5db4613d85052b97f0b2cefd28e457771220f

---
+++ src/main/scala/com/ephox/vault/sql/SqlError.scala
@@ -0,0 +1,125 @@
+package com.ephox
+package vault
+package sql
+
+import scalaz._, Scalaz._
+
+sealed trait SqlError {
+  def exception: java.sql.SQLException =
+    this match {
+      case SqlException(e) => e
+      case SqlWarning(e) => e
+      case DataTruncation(e) => e
+      case BatchUpdateException(e) => e
+    }
+
+  def reason: Option[Cord] =
+    Option(exception.getMessage)
+
+  def state: Option[Cord] =
+    Option(exception.getSQLState)
+
+  def code: Option[Int] =
+    Option(exception.getErrorCode)
+
+  def isSqlException: Boolean =
+    this match {
+      case SqlException(_) => true
+      case SqlWarning(_) => false
+      case DataTruncation(_) => false
+      case BatchUpdateException(_) => false
+    }
+
+  def isWarning: Boolean =
+    this match {
+      case SqlException(_) => false
+      case SqlWarning(_) => true
+      case DataTruncation(_) => false
+      case BatchUpdateException(_) => false
+    }
+
+  def isDataTruncation: Boolean =
+    this match {
+      case SqlException(_) => false
+      case SqlWarning(_) => false
+      case DataTruncation(_) => true
+      case BatchUpdateException(_) => false
+    }
+
+  def isBatchUpdateException: Boolean =
+    this match {
+      case SqlException(_) => false
+      case SqlWarning(_) => false
+      case DataTruncation(_) => false
+      case BatchUpdateException(_) => true
+    }
+
+  def dataSize: Option[Int] =
+    this match {
+      case SqlException(_) => None
+      case SqlWarning(_) => None
+      case DataTruncation(e) => Some(e.getDataSize)
+      case BatchUpdateException(_) => None
+    }
+
+  def index: Option[Int] =
+    this match {
+      case SqlException(_) => None
+      case SqlWarning(_) => None
+      case DataTruncation(e) => Some(e.getIndex)
+      case BatchUpdateException(_) => None
+    }
+
+  def transferSize: Option[Int] =
+    this match {
+      case SqlException(_) => None
+      case SqlWarning(_) => None
+      case DataTruncation(e) => Some(e.getTransferSize)
+      case BatchUpdateException(_) => None
+    }
+
+  def parameter: Option[Boolean] =
+    this match {
+      case SqlException(_) => None
+      case SqlWarning(_) => None
+      case DataTruncation(e) => Some(e.getParameter)
+      case BatchUpdateException(_) => None
+    }
+
+  def read: Option[Boolean] =
+    this match {
+      case SqlException(_) => None
+      case SqlWarning(_) => None
+      case DataTruncation(e) => Some(e.getRead)
+      case BatchUpdateException(_) => None
+    }
+
+  def updateCounts: Option[List[Int]] =
+    this match {
+      case SqlException(_) => None
+      case SqlWarning(_) => None
+      case DataTruncation(_) => None
+      case BatchUpdateException(e) => Some(e.getUpdateCounts.toList)
+    }
+}
+private case class SqlException(ex: java.sql.SQLException) extends SqlError
+private case class SqlWarning(ex: java.sql.SQLWarning) extends SqlError
+private case class DataTruncation(ex: java.sql.DataTruncation) extends SqlError
+private case class BatchUpdateException(ex: java.sql.BatchUpdateException) extends SqlError
+
+object SqlError extends SqlErrorFunctions
+
+trait SqlErrorFunctions {
+  private[sql] def sqlException(e: java.sql.SQLException): SqlError =
+    SqlException(e)
+
+  private[sql] def sqlWarning(e: java.sql.SQLWarning): SqlError =
+    SqlWarning(e)
+
+  private[sql] def dataTruncation(e: java.sql.DataTruncation): SqlError =
+    DataTruncation(e)
+
+  private[sql] def batchUpdateException(e: java.sql.BatchUpdateException): SqlError =
+    BatchUpdateException(e)
+
+}
---
+++ src/main/scala/com/ephox/vault/sql/SqlT.scala
@@ -0,0 +1,123 @@
+package com.ephox
+package vault
+package sql
+
+import scalaz._, Scalaz._
+
+sealed trait SqlT[F[+_], +A] {
+  val run: F[SqlError \/ A]
+
+  def transformer: EitherT[F, SqlError, A] =
+    EitherT(run)
+
+  sealed trait SwitchingSqlT[X] {
+    def v: X
+
+    def <<?:(err: => X)(implicit F: Functor[F]): F[X] =
+      F.map(SqlT.this.run)(_.fold(_ => err, _ => v))
+  }
+
+  def :?>>[X](value: => X): SwitchingSqlT[X] =
+    new SwitchingSqlT[X] {
+      def v = value
+    }
+
+  def isError(implicit F: Functor[F]): F[Boolean] =
+    F.map(run)(_.isLeft)
+
+  def isValue(implicit F: Functor[F]): F[Boolean] =
+    F.map(run)(_.isRight)
+
+  def map[B](f: A => B)(implicit F: Functor[F]): SqlT[F, B] =
+    SqlT(F.map(run)(_ map f))
+
+  def foreach(f: A => Unit)(implicit E: Each[F]): Unit =
+    E.each(run)(_ foreach f)
+
+  def ap[B](f: SqlT[F, A => B])(implicit F: Apply[F]): SqlT[F, B] =
+    SqlT(F(run, f.run)((a, b) => a flatMap (x => b map (_(x)))))
+
+  def flatMap[B](f: A => SqlT[F, B])(implicit F: Monad[F]): SqlT[F, B] =
+    SqlT(F.bind(run)(_.fold(e => F.point(e.left), f(_).run)))
+
+  def traverse[G[+_], B](f: A => G[B])(implicit F: Traverse[F], G: Applicative[G]): G[SqlT[F, B]] =
+    G.map(F.traverse(run)(_ traverse f))(SqlT(_))
+
+  def foldRight[Z](z: => Z)(f: (A, => Z) => Z)(implicit F: Foldable[F]): Z =
+    F.foldRight[SqlError \/ A, Z](run, z)((a, b) => a.foldRight(b)(f))
+
+  def exists(f: A => Boolean)(implicit F: Functor[F]): F[Boolean] =
+    F.map(run)(_ exists f)
+
+  def forall(f: A => Boolean)(implicit F: Functor[F]): F[Boolean] =
+    F.map(run)(_ forall f)
+
+  def toList(implicit F: Functor[F]): F[List[A]] =
+    F.map(run)(_.fold(_ => Nil, List(_)))
+
+  def toStream(implicit F: Functor[F]): F[Stream[A]] =
+    F.map(run)((_: (SqlError \/ A)).fold(_ => Stream(), Stream(_)))
+
+  def toOption(implicit F: Functor[F]): OptionT[F, A] =
+    OptionT.optionT[F](F.map(run)((_: (SqlError \/ A)).toOption))
+
+  def toEither(implicit F: Functor[F]): F[Either[SqlError, A]] =
+    F.map(run)(_.toEither)
+
+  def toValidation(implicit F: Functor[F]): F[Validation[SqlError, A]] =
+    F.map(run)(_.validation)
+
+  def getOrElse[AA >: A](default: => AA)(implicit F: Functor[F]): F[AA] =
+    F.map(run)(_ getOrElse default)
+
+  def |[AA >: A](default: => AA)(implicit F: Functor[F]): F[AA] =
+    getOrElse(default)
+
+  def valueOr[AA >: A](x: SqlError => AA)(implicit F: Functor[F]): F[AA] =
+      F.map(run)(_ valueOr x)
+
+  def orElse[AA >: A](x: => SqlT[F, AA])(implicit F: Bind[F]): SqlT[F, AA] =
+    SqlT(F.bind(run)(_.fold(_ => x.run, _ => run)))
+
+  def |||[AA >: A](x: => SqlT[F, AA])(implicit F: Bind[F]): SqlT[F, AA] =
+    orElse(x)
+}
+
+object SqlT extends SqlTFunctions {
+  def apply[F[+_], A](x: F[SqlError \/ A]): SqlT[F, A] =
+    new SqlT[F, A] {
+      val run = x
+    }
+
+  object Error {
+    def apply[F[+_], A](x: F[SqlError])(implicit F: Functor[F]): SqlT[F, A] =
+      SqlT(F.map(x)(_.left))
+  }
+
+  object Value {
+    def apply[F[+_], A](x: F[A])(implicit F: Functor[F]): SqlT[F, A] =
+      SqlT(F.map(x)(_.right))
+  }
+
+  object Try {
+    def apply[F[+_], A](x: F[() => A])(implicit F: Functor[F]): SqlT[F, A] =
+      SqlT(F.map(x)(a => try {
+        a().right
+      } catch {
+        case e: java.sql.SQLException => SqlError.sqlException(e).left
+        case e: java.sql.SQLWarning => SqlError.sqlWarning(e).left
+        case e: java.sql.DataTruncation => SqlError.dataTruncation(e).left
+        case e: java.sql.BatchUpdateException => SqlError.batchUpdateException(e).left
+      }))
+  }
+
+  object Sql {
+    def apply[A](a: => A): Sql[A] =
+      Try[Id, A](() => a)
+  }
+}
+
+trait SqlTFunctions {
+  type Sql[+A] =
+  SqlT[Id, A]
+}
