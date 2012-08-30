aebfe8534042f47007dd9be1623c0c0b268ba9c1
Tony Morris <tmorris@tmorris.net>
2012-08-30 17:14:46 +1000

    Sql
    Former-commit-id: c29bc38997d4085c7a3130cb394af8dc1886ea25

--- src/main/scala/com/ephox/vault/sql/Statement.scala
+++ src/main/scala/com/ephox/vault/sql/Statement.scala
@@ -2,8 +2,29 @@ package com.ephox
 package vault
 package sql
 
+import SqlT._
+import scalaz._, Scalaz._
+
 sealed trait Statement {
   private[sql] val x: java.sql.Statement
+
+  def addBatch(sql: String): Sql[Unit] =
+    Try(x.addBatch(sql))
+
+  def cancel: Sql[Unit] =
+    Try(x.cancel)
+
+  def clearBatch: Sql[Unit] =
+    Try(x.clearBatch)
+
+  def clearWarnings: Sql[Unit] =
+    Try(x.clearWarnings)
+
+  def close: Sql[Unit] =
+    Try(x.close)
+
+  def execute(sql: String): Sql[Boolean] =
+    Try(x.execute(sql))
 }
 
 object Statement {
