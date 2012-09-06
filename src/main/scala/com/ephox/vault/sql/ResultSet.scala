e3eaf2f13c2f3e2b734eafb9037901d804a9389c
Tony Morris <tmorris@tmorris.net>
2012-09-06 12:19:05 +1000

    get/set
    Former-commit-id: 3c757123cf4442311057ccc0132d34af11d4952b

--- src/main/scala/com/ephox/vault/sql/ResultSet.scala
+++ src/main/scala/com/ephox/vault/sql/ResultSet.scala
@@ -38,59 +38,35 @@ sealed trait ResultSet {
   def first: Sql[Boolean] =
     Try(x.first)
 
-  def array(q: Int \/ String): XSql[Array] =
-    XTry(q.fold(
-      x.getArray(_)
-    , x.getArray(_)
-    )) map (Array(_))
+  def array(q: Column): XGetSet[Array] =
+    XGetSet(q, Array(q.fold(x getArray _, x getArray _)), a => q.fold(i => x.updateArray(i, a.x), n => x.updateArray(n, a.x)))
 
-  def asciiStream(q: Int \/ String): XSql[java.io.InputStream] =
-    XTry(q.fold(
-      x.getAsciiStream(_)
-    , x.getAsciiStream(_)
-    ))
+  def asciiStream(q: Column): XGetSet[java.io.InputStream] =
+    XGetSet(q, q.fold(x getAsciiStream _, x getAsciiStream _), a => q.fold(i => x.updateAsciiStream(i, a), n => x.updateAsciiStream(n, a)))
 
-  def bigDecimal(q: Int \/ String): XSql[BigDecimal] =
-    XTry(q.fold(
-      x.getBigDecimal(_)
-    , x.getBigDecimal(_)
-    ))
+  def bigDecimal(q: Column): XGetSet[java.math.BigDecimal] =
+    XGetSet(q, q.fold(x getBigDecimal _, x getBigDecimal _), a => q.fold(i => x.updateBigDecimal(i, a), n => x.updateBigDecimal(n, a)))
 
-  def binaryStream(q: Int \/ String): XSql[java.io.InputStream] =
-    XTry(q.fold(
-      x.getBinaryStream(_)
-    , x.getBinaryStream(_)
-    ))
+  def binaryStream(q: Column): XGetSet[java.io.InputStream] =
+    XGetSet(q, q.fold(x getBinaryStream _, x getBinaryStream _), a => q.fold(i => x.updateBinaryStream(i, a), n => x.updateBinaryStream(n, a)))
 
-  def blob(q: Int \/ String): XSql[Blob] =
-    XTry(q.fold(
-      x.getBlob(_)
-    , x.getBlob(_)
-    )) map (Blob(_))
+  def blob(q: Column): XGetSet[Blob] =
+    XGetSet(q, Blob(q.fold(x getBlob _, x getBlob _)), a => q.fold(i => x.updateBlob(i, a.x), n => x.updateBlob(n, a.x)))
 
-  def boolean(q: Column): XGetSet[Boolean] =
-    XGetSet(q, q.fold(x getBoolean _, x getBoolean _), a => q.fold(i => x.updateBoolean(i, a), n => x.updateBoolean(n, a)))
+  def boolean(q: Column): GetSet[Boolean] =
+    GetSet(q, q.fold(x getBoolean _, x getBoolean _), a => q.fold(i => x.updateBoolean(i, a), n => x.updateBoolean(n, a)))
 
-  def byte(q: Int \/ String): Sql[Byte] =
-    Try(q.fold(
-      x.getByte(_)
-    , x.getByte(_)
-    ))
+  def byte(q: Column): GetSet[Byte] =
+    GetSet(q, q.fold(x getByte _, x getByte _), a => q.fold(i => x.updateByte(i, a), n => x.updateByte(n, a)))
 
   def bytes(q: Column): XGetSet[scala.Array[Byte]] =
     XGetSet(q, q.fold(x getBytes _, x getBytes _), a => q.fold(i => x.updateBytes(i, a), n => x.updateBytes(n, a)))
 
-  def characterStream(q: Int \/ String): XSql[java.io.Reader] =
-    XTry(q.fold(
-      x.getCharacterStream(_)
-    , x.getCharacterStream(_)
-    ))
+  def characterStream(q: Column): XGetSet[java.io.Reader] =
+    XGetSet(q, q.fold(x getCharacterStream _, x getCharacterStream _), a => q.fold(i => x.updateCharacterStream(i, a), n => x.updateCharacterStream(n, a)))
 
-  def clob(q: Int \/ String): XSql[Clob] =
-    XTry(q.fold(
-      x.getClob(_)
-    , x.getClob(_)
-    )) map (Clob(_))
+  def clob(q: Column): XGetSet[Clob] =
+    XGetSet(q, Clob(q.fold(x getClob _, x getClob _)), a => q.fold(i => x.updateClob(i, a.x), n => x.updateClob(n, a.x)))
 
   def concurrency: Sql[ResultSetConcurrency] =
     Try(x.getConcurrency) map (c =>
@@ -105,23 +81,23 @@ sealed trait ResultSet {
   def cursorName: Sql[String] =
     Try(x.getCursorName)
 
-  def date(q: Int \/ String, m: Option[java.util.Calendar]): XSql[Date] =
-    XTry(q.fold(
+  def date(q: Column, m: Option[java.util.Calendar]): XGetSet[Date] =
+    XGetSet(q, Date(q.fold(
       i => m match {
         case None => x.getDate(i)
         case Some(r) => x.getDate(i, r)
       }
-      , n => m match {
-        case None => x.getDate(n)
-        case Some(r) => x.getDate(n, r)
-      }
-    )) map (Date(_))
-
-  def double(q: Int \/ String): Sql[Double] =
-    Try(q.fold(
-      x.getDouble(_)
-    , x.getDouble(_)
+    , n => m match {
+      case None => x.getDate(n)
+      case Some(r) => x.getDate(n, r)
+    })
+    ), t => q.fold(
+      i => x.updateDate(i, t.x)
+    , n => x.updateDate(n, t.x)
     ))
+
+  def double(q: Column): GetSet[Double] =
+    GetSet(q, q.fold(x getDouble _, x getDouble _), a => q.fold(i => x.updateDouble(i, a), n => x.updateDouble(n, a)))
 
   def fetchDirection: Sql[FetchDirection] =
     Try(x.getFetchDirection) map (c =>
@@ -138,83 +114,74 @@ sealed trait ResultSet {
   def fetchSize: Sql[Int] =
     Try(x.getFetchSize)
 
-  def float(q: Int \/ String): Sql[Float] =
-    Try(q.fold(
-      x.getFloat(_)
-    , x.getFloat(_)
-    ))
+  def float(q: Column): GetSet[Float] =
+    GetSet(q, q.fold(x getFloat _, x getFloat _), a => q.fold(i => x.updateFloat(i, a), n => x.updateFloat(n, a)))
 
-  def int(q: Int \/ String): Sql[Int] =
-    Try(q.fold(
-      x.getInt(_)
-    , x.getInt(_)
-    ))
+  def int(q: Column): GetSet[Int] =
+    GetSet(q, q.fold(x getInt _, x getInt _), a => q.fold(i => x.updateInt(i, a), n => x.updateInt(n, a)))
 
-  def long(q: Int \/ String): Sql[Long] =
-    Try(q.fold(
-      x.getLong(_)
-    , x.getLong(_)
-    ))
+  def long(q: Column): GetSet[Long] =
+    GetSet(q, q.fold(x getLong _, x getLong _), a => q.fold(i => x.updateLong(i, a), n => x.updateLong(n, a)))
 
   def metaData: Sql[ResultSetMetaData] =
     Try(ResultSetMetaData(x.getMetaData))
 
-  def obj(q: Int \/ String, m: Option[collection.mutable.Map[String, Class[_]]]): XSql[AnyRef] =
-    XTry(q.fold(
+  def obj(q: Column, m: Option[collection.mutable.Map[String, Class[_]]]): XGetSet[AnyRef] =
+    XGetSet(q, q.fold(
       i => m match {
         case None => x.getObject(i)
         case Some(r) => x.getObject(i, r)
       }
-      , n => m match {
-        case None => x.getObject(n)
-        case Some(r) => x.getObject(n, r)
-      }
+    , n => m match {
+      case None => x.getObject(n)
+      case Some(r) => x.getObject(n, r)
+    })
+    , t => q.fold(
+      i => x.updateObject(i, t)
+    , n => x.updateObject(n, t)
     ))
 
-  def ref(q: Int \/ String): XSql[Ref] =
-    XTry(q.fold(
-      x.getRef(_)
-    , x.getRef(_)
-    )) map (Ref(_))
+  def ref(q: Column): XGetSet[Ref] =
+    XGetSet(q, Ref(q.fold(x getRef _, x getRef _)), a => q.fold(i => x.updateRef(i, a.x), n => x.updateRef(n, a.x)))
 
-  def short(q: Int \/ String): Sql[Short] =
-    Try(q.fold(
-      x.getShort(_)
-    , x.getShort(_)
-    ))
+  def short(q: Column): GetSet[Short] =
+    GetSet(q, q.fold(x getShort _, x getShort _), a => q.fold(i => x.updateShort(i, a), n => x.updateShort(n, a)))
 
   def statement: Sql[Statement] =
     Try(Statement(x.getStatement))
 
-  def string(q: Int \/ String): XSql[String] =
-    XTry(q.fold(
-      x.getString(_)
-    , x.getString(_)
-    ))
+  def string(q: Column): XGetSet[String] =
+    XGetSet(q, q.fold(x getString _, x getString _), a => q.fold(i => x.updateString(i, a), n => x.updateString(n, a)))
 
-  def time(q: Int \/ String, m: Option[java.util.Calendar]): XSql[Time] =
-    XTry(q.fold(
+  def time(q: Column, m: Option[java.util.Calendar]): XGetSet[Time] =
+    XGetSet(q, Time(q.fold(
       i => m match {
         case None => x.getTime(i)
         case Some(r) => x.getTime(i, r)
       }
-      , n => m match {
-        case None => x.getTime(n)
-        case Some(r) => x.getTime(n, r)
-      }
-    )) map (Time(_))
+    , n => m match {
+      case None => x.getTime(n)
+      case Some(r) => x.getTime(n, r)
+    })
+    ), t => q.fold(
+      i => x.updateTime(i, t.x)
+    , n => x.updateTime(n, t.x)
+    ))
 
-  def timestamp(q: Int \/ String, m: Option[java.util.Calendar]): XSql[Timestamp] =
-    XTry(q.fold(
+  def timestamp(q: Column, m: Option[java.util.Calendar]): XGetSet[Timestamp] =
+    XGetSet(q, Timestamp(q.fold(
       i => m match {
         case None => x.getTimestamp(i)
         case Some(r) => x.getTimestamp(i, r)
       }
-      , n => m match {
-        case None => x.getTimestamp(n)
-        case Some(r) => x.getTimestamp(n, r)
-      }
-    )) map (Timestamp(_))
+    , n => m match {
+      case None => x.getTimestamp(n)
+      case Some(r) => x.getTimestamp(n, r)
+    })
+    ), t => q.fold(
+      i => x.updateTimestamp(i, t.x)
+    , n => x.updateTimestamp(n, t.x)
+    ))
 
   def ty: Sql[ResultSetType] =
     Try(x.getType) map (c =>
@@ -228,7 +195,7 @@ sealed trait ResultSet {
         sys.error("[" + c + """] http://docs.oracle.com/javase/1.5.0/docs/api/java/sql/ResultSet.html#getType%28%29""")
       )
 
-  def url(q: Int \/ String): XSql[java.net.URL] =
+  def url(q: Column): XSql[java.net.URL] =
     XTry(q.fold(
       x.getURL(_)
     , x.getURL(_)
