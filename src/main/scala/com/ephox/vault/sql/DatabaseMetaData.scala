3f428b06c8076ce1b6f962433853f81e9a17c1de
Tony Morris <tmorris@tmorris.net>
2012-09-10 12:39:26 +1000

    database metadata
    Former-commit-id: 10720ddeec0ab15ac539091ffcd4c4d17692dc15

--- src/main/scala/com/ephox/vault/sql/DatabaseMetaData.scala
+++ src/main/scala/com/ephox/vault/sql/DatabaseMetaData.scala
@@ -2,8 +2,31 @@ package com.ephox
 package vault
 package sql
 
+import SqlT._
+
 sealed trait DatabaseMetaData {
   private[sql] val x: java.sql.DatabaseMetaData
+
+  def allProceduresAreCallable: Sql[Boolean] =
+    Try(x.allProceduresAreCallable)
+
+  def allTablesAreSelectable: Sql[Boolean] =
+    Try(x.allTablesAreSelectable)
+
+  def dataDefinitionCausesTransactionCommit: Sql[Boolean] =
+    Try(x.dataDefinitionCausesTransactionCommit)
+
+  def dataDefinitionIgnoredInTransactions: Sql[Boolean] =
+    Try(x.dataDefinitionIgnoredInTransactions)
+
+  def deletesAreDetectable(t: ResultSetType): Sql[Boolean] =
+    Try(x.deletesAreDetected(t.int))
+
+  def doesMaxRowSizeIncludeBlobs: Sql[Boolean] =
+    Try(x.doesMaxRowSizeIncludeBlobs)
+
+  def getAttributes(catalog: Option[String], schemaPattern: Option[String], typeNamePattern: String, attributeNamePattern: String): Sql[ResultSet] =
+    error("")
 }
 
 object DatabaseMetaData {
